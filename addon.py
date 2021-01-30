59a796cd304144feeca701393e4cf05f91b34368
François Lavaud-Wernert <francois@lavaud.family>
2021-01-30 23:30:50 +0100

    Cleaning

--- addon.py
+++ addon.py
@@ -1,14 +1,21 @@
 import json, urllib2, urlparse, sys
 import xbmc, xbmcaddon, xbmcgui, xbmcplugin
 
-ADDON_NAME       = xbmcaddon.Addon().getAddonInfo('name')
-CHANNEL_INFO_URL = 'https://chaines-tv.orange.fr/live-webapp/v3/applications/PC/users/me/channels/%s/stream?terminalModel=WEB_PC&terminalId=Windows10-x64-Firefox-GVASQGFQ6QJNUW5OJJICWHPI7766H7VWLMPIOZYKAUS6DCM2LMOA'
+ADDON_NAME          = xbmcaddon.Addon().getAddonInfo('name')
+USER_AGENT          = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
+HOST                = 'chaines-tv.orange.fr'
+CHANNEL_INFO_URL    = 'https://chaines-tv.orange.fr/live-webapp/v3/applications/PC/users/me/channels/{}/stream?terminalModel=WEB_PC&terminalId=Windows10-x64-Firefox-GVASQGFQ6QJNUW5OJJICWHPI7766H7VWLMPIOZYKAUS6DCM2LMOA'
 
 def log(msg, level):
     xbmc.log('{}: {}'.format(ADDON_NAME, msg), level)
 
 def get_channel_info(channel_id):
-    channel_info = urllib2.urlopen(CHANNEL_INFO_URL % channel_id).read()
+    request = urllib2.Request(CHANNEL_INFO_URL.format(channel_id), headers={
+        'User-Agent': USER_AGENT,
+        'Host': HOST
+    })
+
+    channel_info = urllib2.urlopen(request).read()
     channel_info = json.loads(channel_info)
 
     stream_info = {
@@ -25,14 +32,18 @@ def get_channel_info(channel_id):
 
 def get_channel_stream_item(channel_id):
     channel_info = get_channel_info(channel_id)
-    license_key = '{}|{}|{}'.format(channel_info['laUrl'], 'User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0', 'B{SSM}')
+
+    license_server_url = channel_info['laUrl']
+    headers = 'User-Agent={}&Host={}'.format(USER_AGENT, HOST)
+    post_data = 'R{SSM}'
+    response = 'B'
+
+    license_key = '{}|{}|{}|{}'.format(license_server_url, headers, post_data, response)
     log(license_key, xbmc.LOGNOTICE)
 
     listitem = xbmcgui.ListItem(path=channel_info['path'])
-
-    # listitem.setMimeType('application/xml+dash')
-    # listitem.setContentLookup(False)
-
+    listitem.setMimeType('application/xml+dash')
+    listitem.setContentLookup(False)
     listitem.setProperty('inputstreamaddon', 'inputstream.adaptive')
     listitem.setProperty('inputstream.adaptive.manifest_type', 'mpd')
     listitem.setProperty('inputstream.adaptive.license_type', 'com.widevine.alpha')
@@ -43,7 +54,7 @@ def get_channel_stream_item(channel_id):
 def main():
     if(len(sys.argv) > 3):
         args = urlparse.parse_qs(sys.argv[2][1:])
-        log('Loading channel #{}'.format(args['channel_id'][0]), xbmc.LOGNOTICE)
+        log('Loading channel {}'.format(args['channel_id'][0]), xbmc.LOGNOTICE)
         get_channel_stream_item(args['channel_id'][0])
 
 if __name__ == '__main__':
