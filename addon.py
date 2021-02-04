4b58c634038575314cb1980ca000b5c0019d9859
François Lavaud-Wernert <francois@lavaud.family>
2021-02-05 00:20:24 +0100

    Merge addon et script settings

--- addon.py
+++ addon.py
@@ -2,11 +2,8 @@
 import json, urllib2, urlparse, sys
 import xbmc, xbmcaddon, xbmcgui, xbmcplugin
 
+from resources.lib import CHANNEL_INFO_URL, HOST, USER_AGENT
 from resources.lib.utils import log
-
-USER_AGENT          = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
-HOST                = 'chaines-tv.orange.fr'
-CHANNEL_INFO_URL    = 'https://chaines-tv.orange.fr/live-webapp/v3/applications/PC/users/me/channels/{}/stream?terminalModel=WEB_PC&terminalId=Windows10-x64-Firefox-GVASQGFQ6QJNUW5OJJICWHPI7766H7VWLMPIOZYKAUS6DCM2LMOA'
 
 def get_channel_info(channel_id):
     req = urllib2.Request(CHANNEL_INFO_URL.format(channel_id), headers={
