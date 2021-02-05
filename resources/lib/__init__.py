fa4a3660315a0748e0010e145af835a3cacfc6ea
François Lavaud-Wernert <francois@lavaud.family>
2021-02-05 19:09:26 +0100

    Enable all Orange free channels

--- resources/lib/__init__.py
+++ resources/lib/__init__.py
@@ -1,5 +1,5 @@
-ORANGE_HOME_URL     = 'https://chaines-tv.orange.fr/'
-CHANNEL_INFO_URL    = 'https://chaines-tv.orange.fr/live-webapp/v3/applications/PC/users/me/channels/{}/stream?terminalModel=WEB_PC&terminalId=Windows10-x64-Firefox-GVASQGFQ6QJNUW5OJJICWHPI7766H7VWLMPIOZYKAUS6DCM2LMOA'
+CHANNELS_ENDPOINT       = 'https://rp-live.orange.fr/live-webapp/v3/applications/PC/channels'
+STREAM_INFO_ENDPOINT    = 'https://chaines-tv.orange.fr/live-webapp/v3/applications/PC/users/me/channels/{}/stream?terminalModel=WEB_PC'
 
-USER_AGENT          = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
-HOST                = 'chaines-tv.orange.fr'
+USER_AGENT              = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
+HOST                    = 'chaines-tv.orange.fr'
--- resources/lib/m3u_generator.py
+++ resources/lib/m3u_generator.py
@@ -1,71 +1,45 @@
-from urllib import request
+# -*- coding: utf-8 -*-
+import json
+from urllib import request, parse
 from html.parser import HTMLParser
 
-from __init__ import HOST, ORANGE_HOME_URL, USER_AGENT
-
-CHANNEL_DIV_CLASSNAME = 'list-item-wrapper wptv-xsmall-1 wptv-medium-3 wptv-large-4'
-CHANNEL_IMG_CLASSNAME = 'poster-logo bottom-left'
+from __init__ import CHANNELS_ENDPOINT, USER_AGENT
 
 M3U_FILEPATH = '../orange-fr.m3u'
-M3U_INFO_LINE = '#EXTINF:-1 tvg-id="C{}.api.telerama.fr" tvg-logo="{}",{}'
-M3U_URL_LINE = 'plugin://script.orange.fr/?channel_id={}'
 
-CHANNELS = {}
-
-class Parser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.channel_id = False
-
-    def handle_starttag(self, tag, attrs):
-        if(tag == 'div'):
-            new_attrs = parse_attrs(attrs)
-
-            if(new_attrs.get('class') == CHANNEL_DIV_CLASSNAME):
-                self.channel_id = new_attrs['id']
-
-        if(tag == 'img'):
-            new_attrs = parse_attrs(attrs)
-
-            if(new_attrs.get('class') == CHANNEL_IMG_CLASSNAME):
-                CHANNELS[self.channel_id] = {
-                    'img_url': new_attrs['src'],
-                    'name': new_attrs['alt']
-                }
-
-def parse_attrs(attrs):
-    new_attrs = {}
-
-    for attr in attrs:
-        new_attrs[attr[0]] = attr[1]
-    
-    return new_attrs
-
-def get_channels_info():
-    req = request.Request(ORANGE_HOME_URL, headers={
+def get_channels():
+    req = request.Request(CHANNELS_ENDPOINT, headers={
         'User-Agent': USER_AGENT,
-        'Host': HOST
+        'Host': parse.urlparse(CHANNELS_ENDPOINT).netloc
     })
 
     res = request.urlopen(req).read().decode('utf-8')
+    channels = json.loads(res)
+    channels.sort(key=lambda x: x.get('zappingNumber'))
 
-    parser = Parser()
-    parser.feed(res)
+    return channels
 
-def write_m3u():
+def channel_entry(channel):
+    entry = '##\t' + channel['name'] + '\n' \
+        + '##\t' + channel['name'].lower() + '\n' \
+        + '#EXTINF:-1 tvg-id="C' + channel['id'] + '.api.telerama.fr" tvg-logo="' + channel['logos']['square'] + '",' + channel['name'] + '\n' \
+        + 'plugin://script.orange.fr/?channel_id=' + channel['id'] + '\n' \
+        + '\n'
+
+    return entry
+
+def write_m3u(channels):
     file = open(M3U_FILEPATH, "w")
     file.write('#EXTM3U tvg-shift=0' + '\n\n')
 
-    for id, channel in CHANNELS.items():
-        file.write('##\t' + channel['name'] + '\n')
-        file.write('##\t' + channel['name'].lower() + '\n')
-        file.write(M3U_INFO_LINE.format(id, channel['img_url'], channel['name']) + '\n')
-        file.write(M3U_URL_LINE.format(id) + '\n')
-        file.write('\n')
+    for channel in channels:
+        file.write(channel_entry(channel))
+
+    file.close()
 
 def generate_m3u():
-    get_channels_info()
-    write_m3u()
+    channels = get_channels()
+    write_m3u(channels)
 
 def main():
     generate_m3u()
