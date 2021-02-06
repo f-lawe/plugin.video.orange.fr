95407c27fec58b7aba1cce6f8c2bc04908ba82ff
François Lavaud-Wernert <francois@lavaud.family>
2021-02-06 22:47:18 +0100

    Adding placeholders

--- resources/lib/m3u_generator.py
+++ resources/lib/m3u_generator.py
@@ -19,26 +19,55 @@ def get_channels():
 
     return channels
 
-def channel_entry(channel):
-    entry = '##\t' + channel['name'] + '\n' \
-        + '##\t' + channel['name'].lower() + '\n' \
-        + '#EXTINF:-1 tvg-id="C' + channel['id'] + '.api.telerama.fr" tvg-logo="' + channel['logos']['square'] + '",' + channel['name'] + '\n' \
-        + 'plugin://script.orange.fr/?channel_id=' + channel['id'] + '\n' \
-        + '\n'
-
-    return entry
-
-def write_m3u(channels):
-    file = open(M3U_FILEPATH, "w")
-    file.write('#EXTM3U tvg-shift=0' + '\n\n')
+def load_channels(channels):
+    current_zapping_number = 1
+    loaded_channels = []
 
     for channel in channels:
-        file.write(channel_entry(channel))
+        next_zapping_number = channel['zappingNumber']
+
+        for zapping_number in range(current_zapping_number, next_zapping_number)[:-1]:
+            loaded_channels.append(None)
+
+        loaded_channels.append(channel)
+        current_zapping_number = next_zapping_number
+
+    return loaded_channels
+
+def channel_entry(channel):
+    return """
+##\t{name}
+#EXTINF:-1 tvg-name="{zapping_number}" tvg-id="C{id}.api.telerama.fr" tvg-logo="{logo}" group-title="channels",{name}
+plugin://script.orange.fr/?channel_id={id}
+""".format(
+        id=channel['id'],
+        name=channel['name'],
+        logo=channel['logos']['square'],
+        zapping_number=channel['zappingNumber'])
+
+def empty_entry(zapping_number):
+    return """
+##\tPLACEHOLDER
+#EXTINF:-1 tvg-name="{zapping_number}" tvg-id="" tvg-logo="" group-title="-",
+http://null
+""".format(
+        zapping_number=zapping_number)
+
+def write_m3u(channels):
+    file = open(M3U_FILEPATH, "wb")
+    file.write('#EXTM3U tvg-shift=0\n'.encode('utf-8'))
+
+    for zapping_number, channel in enumerate(channels):
+        if channel == None:
+            file.write(empty_entry(zapping_number).encode('utf-8'))
+        else:
+            file.write(channel_entry(channel).encode('utf-8'))
 
     file.close()
 
 def generate_m3u():
     channels = get_channels()
+    channels = load_channels(channels)
     write_m3u(channels)
 
 def main():
