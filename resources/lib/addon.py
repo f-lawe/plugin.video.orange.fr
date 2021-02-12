ea0255cf4b211eba83e27dc6e3cf2e3e3961e0f2
François Lavaud-Wernert <francois@lavaud.family>
2021-02-12 23:03:37 +0100

    Code cleaning

--- resources/lib/addon.py
+++ resources/lib/addon.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-import urlparse as parse
+from urlparse import urlparse, parse_qs, urlunparse
+from urllib import urlencode
+
 import xbmc, xbmcgui, xbmcplugin
 import routing
 
@@ -9,9 +11,10 @@ from utils import dialog, log
 plugin = routing.Plugin()
 
 def extract_widevine_info(channel_stream):
+    """Extract Widevine licence information from stream details"""
     path = channel_stream.get('url')
-    laUrl = None
 
+    laUrl = None
     for system in channel_stream.get('protectionData'):
         if system.get('keySystem') == 'com.widevine.alpha':
             laUrl = system.get('laUrl')
@@ -20,6 +23,7 @@ def extract_widevine_info(channel_stream):
 
 @plugin.route('/channel/<channel_id>')
 def channel(channel_id):
+    """Load stream for the required channel id"""
     stream = get_channel_stream(channel_id)
 
     if stream == False:
@@ -27,11 +31,12 @@ def channel(channel_id):
         return
 
     path, license_server_url = extract_widevine_info(stream)
-    headers = 'Content-Type=&User-Agent={}&Host={}'.format(USER_AGENT, parse.urlparse(license_server_url).netloc)
+    headers = 'Content-Type=&User-Agent={}&Host={}'.format(USER_AGENT, urlparse(license_server_url).netloc)
     post_data = 'R{SSM}'
     response = ''
-
     license_key = '{}|{}|{}|{}'.format(license_server_url, headers, post_data, response)
+
+    log(path, xbmc.LOGNOTICE)
     log(license_key, xbmc.LOGNOTICE)
 
     listitem = xbmcgui.ListItem(path=path)
@@ -46,10 +51,12 @@ def channel(channel_id):
 
 @plugin.route('/iptv/channels')
 def iptv_channels():
+    """Return JSON-STREAMS formatted data for all live channels"""
     pass
 
 @plugin.route('/iptv/epg')
 def iptv_epg():
+    """Return JSON-EPG formatted data for all live channel EPG data"""
     pass
 
 if __name__ == '__main__':
