3a925bb5a964e113c177b8bc3d64351a5ae0263a
François Lavaud-Wernert <francois@lavaud.family>
2021-02-13 21:06:34 +0100

    Apply pylint

--- resources/lib/orange.py
+++ resources/lib/orange.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-import json, sys
+import json
+import sys
 
 if sys.version_info[0] < 3:
-    from urlparse import urlparse
-    from urllib2 import Request, urlopen
+    from urlparse import urlparse # pylint: disable=import-error
+    from urllib2 import HTTPError, Request, urlopen # pylint: disable=import-error
 else:
+    from urllib.error import HTTPError
     from urllib.parse import urlparse
     from urllib.request import Request, urlopen
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
 
 def get_channels():
+    """get_channels"""
     endpoint = 'https://rp-live.orange.fr/live-webapp/v3/applications/PC/channels'
 
     req = Request(endpoint, headers={
@@ -22,6 +25,7 @@ def get_channels():
     return json.loads(res.read())
 
 def get_channel_stream(channel_id):
+    """get_channel_stream"""
     endpoint = 'https://chaines-tv.orange.fr/live-webapp/v3/applications/PC/users/me/channels/{}/stream?terminalModel=WEB_PC'
 
     req = Request(endpoint.format(channel_id), headers={
@@ -31,13 +35,14 @@ def get_channel_stream(channel_id):
 
     try:
         res = urlopen(req)
-    except request.HTTPError as error:
+    except HTTPError as error:
         if error.code == 403:
             return False
 
     return json.loads(res.read())
 
 def get_programs(period):
+    """get_programs"""
     # endpoint = 'https://rp-live.orange.fr/live-webapp/v3/applications/PC/programs?period={},{}&mco=OFR'
     endpoint = 'https://rp-live.orange.fr/live-webapp/v3/applications/PC/programs?period={}&mco=OFR'
 
