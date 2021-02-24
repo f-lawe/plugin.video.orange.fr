6c3d182a3889171b02d0d89a6e8e08e50caca343
BreizhReloaded <breizhreloaded@outlook.com>
2021-02-24 12:50:47 +0100

    Fully remove Python 2

--- resources/lib/orange.py
+++ resources/lib/orange.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 '''Orange API client'''
 import json
-import sys
-
-if sys.version_info[0] < 3:
-    from urlparse import urlparse # pylint: disable=import-error
-    from urllib2 import HTTPError, Request, urlopen # pylint: disable=import-error
-else:
-    from urllib.error import HTTPError
-    from urllib.parse import urlparse
-    from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+from urllib.parse import urlparse
+from urllib.request import Request, urlopen
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
 
@@ -23,14 +17,12 @@ def get_channels():
     })
 
     res = urlopen(req)
-    
     return json.loads(res.read())
 
 def get_channel_stream(channel_id):
     '''Get stream information (MPD address, Widewine key) for the specified channel'''
     endpoint = \
         'https://mediation-tv.orange.fr/all/live/v3/applications/PC/users/me/channels/{}/stream?terminalModel=WEB_PC'
-        # 'https://chaines-tv.orange.fr/live-webapp/v3/applications/PC/users/me/channels/{}/stream?terminalModel=WEB_PC'
 
     req = Request(endpoint.format(channel_id), headers={
         'User-Agent': USER_AGENT,
