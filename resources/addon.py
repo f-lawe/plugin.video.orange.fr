bf202463ec3da38972137716f2b6808538eb9624
François Lavaud-Wernert <francois@lavaud.family>
2024-07-05 18:35:38 +0200

    Refactoring

--- resources/addon.py
+++ resources/addon.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """Addon entry point"""
+from datetime import date, datetime
+
 import routing # pylint: disable=import-error
 import xbmc
 import xbmcplugin
 
-from lib.channel import build_channel_listitem
+from lib.channel import build_channel_listitem, format_inputstream_properties
 from lib.iptvmanager import IPTVManager
-from lib.utils import log
+from lib.orange import get_channel_stream, get_channels, get_programs
+from lib.utils import dialog, log
 
 plugin = routing.Plugin()
 
@@ -14,7 +17,18 @@ plugin = routing.Plugin()
 def channel(channel_id):
     """Load stream for the required channel id"""
     log('Loading channel {}'.format(channel_id), xbmc.LOGINFO)
-    listitem = build_channel_listitem(channel_id)
+
+    drm = 'widevine'
+    stream = get_channel_stream(channel_id)
+
+    if not stream:
+        dialog('This channel is not part of your current registration.')
+        return
+
+    path, manifest_type, license_key = format_inputstream_properties(stream, drm)
+    log(license_key, xbmc.LOGDEBUG)
+
+    listitem = build_channel_listitem(path, manifest_type, drm, license_key)
     xbmcplugin.setResolvedUrl(plugin.handle, True, listitem=listitem)
 
 @plugin.route('/iptv/channels')
@@ -22,14 +36,24 @@ def iptv_channels():
     """Return JSON-STREAMS formatted data for all live channels"""
     log('Loading channel data for IPTV Manager', xbmc.LOGINFO)
     port = int(plugin.args.get('port')[0])
-    IPTVManager(port).send_channels()
+    IPTVManager(port).send_channels(get_channels())
 
 @plugin.route('/iptv/epg')
 def iptv_epg():
     """Return JSON-EPG formatted data for all live channel EPG data"""
     log('Loading progam data for IPTV Manager', xbmc.LOGINFO)
+
+    today = datetime.timestamp(datetime.combine(date.today(), datetime.min.time()))
+    day_duration = 24 * 60 * 60
+    programs = []
+
+    for day in range(0, 6):
+        period_start = (today + day_duration * day) * 1000
+        period_end = period_start + (day_duration * 1000)
+        programs.extend(get_programs(period_start=period_start, period_end=period_end))
+
     port = int(plugin.args.get('port')[0])
-    IPTVManager(port).send_epg()
+    IPTVManager(port).send_epg(programs)
 
 if __name__ == '__main__':
     plugin.run()
