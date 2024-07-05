de761415ce97188ba010fdd066f19e401cea481d
François Lavaud-Wernert <francois@lavaud.family>
2024-07-05 18:36:06 +0200

    Refactor provider wrapper

--- resources/lib/providers/__init__.py
+++ resources/lib/providers/__init__.py
@@ -4,4 +4,18 @@ from .provider_interface import ProviderInterface
 
 from .fr import OrangeFranceProvider
 
-Provider = OrangeFranceProvider
+class _ProviderWrapper: # pylint: disable=too-few-public-methods
+    """PROVIDER WRAPPER"""
+
+    def __init__(self):
+        self.__provider = OrangeFranceProvider()
+
+    def get_instance(self):
+        """GETTER"""
+        return self.__provider
+
+_WRAPPER = _ProviderWrapper()
+
+def provider():
+    """Return the current provider """
+    return _WRAPPER.get_instance()
