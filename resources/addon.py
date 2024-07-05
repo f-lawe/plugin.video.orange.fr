90e030b527e7ed06ad08ff7e7d1163eed3e6daf6
François Lavaud-Wernert <francois@lavaud.family>
2024-07-05 18:35:11 +0200

    Move channel logic to dedicated module

--- resources/addon.py
+++ resources/addon.py
@@ -1,55 +1,19 @@
 # -*- coding: utf-8 -*-
 '''Addon entry point'''
-from urllib.parse import urlparse
-
 import routing # pylint: disable=import-error
 import xbmc
-import xbmcgui
 import xbmcplugin
 
-from lib.orange import get_channel_stream, USER_AGENT
-from lib.utils import dialog, log
+from lib.channel import build_channel_listitem
+from lib.utils import log
 
 plugin = routing.Plugin()
-
-def extract_stream_info(license_type, channel_stream):
-    '''Extract Widevine licence information from stream details'''
-    path = channel_stream.get('url')
-
-    license_server_url = None
-    for system in channel_stream.get('protectionData'):
-        if system.get('keySystem') == license_type:
-            license_server_url = system.get('laUrl')
-
-    return path, license_server_url
 
 @plugin.route('/channel/<channel_id>')
 def channel(channel_id):
     '''Load stream for the required channel id'''
     log('Loading channel {}'.format(channel_id), xbmc.LOGINFO)
-    stream = get_channel_stream(channel_id)
-
-    if not stream:
-        dialog('This channel is not part of your current registration.')
-        return
-
-    license_type = 'com.widevine.alpha'
-    path, license_server_url = extract_stream_info(license_type, stream)
-    headers = 'Content-Type=&User-Agent={}&Host={}'.format(USER_AGENT, urlparse(license_server_url).netloc)
-    post_data = 'R{SSM}'
-    response = ''
-    license_key = '{}|{}|{}|{}'.format(license_server_url, headers, post_data, response)
-
-    log(license_key, xbmc.LOGDEBUG)
-
-    listitem = xbmcgui.ListItem(path=path)
-    listitem.setMimeType('application/xml+dash')
-    listitem.setContentLookup(False)
-    listitem.setProperty('inputstream', 'inputstream.adaptive')
-    listitem.setProperty('inputstream.adaptive.manifest_type', 'mpd')
-    listitem.setProperty('inputstream.adaptive.license_type', license_type)
-    listitem.setProperty('inputstream.adaptive.license_key', license_key)
-
+    listitem = build_channel_listitem(channel_id)
     xbmcplugin.setResolvedUrl(plugin.handle, True, listitem=listitem)
 
 @plugin.route('/iptv/channels')
---
+++ resources/lib/channel.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+'''Channel stream management'''
+from urllib.parse import urlparse
+
+import inputstreamhelper # pylint: disable=import-error
+import xbmc
+import xbmcgui
+
+from .orange import get_channel_stream, USER_AGENT
+from .utils import dialog, log
+
+def extract_stream_info(license_type, channel_stream):
+    '''Extract Widevine licence information from stream details'''
+    path = channel_stream.get('url')
+
+    license_server_url = None
+    for system in channel_stream.get('protectionData'):
+        if system.get('keySystem') == license_type:
+            license_server_url = system.get('laUrl')
+
+    return path, license_server_url
+
+def format_inputstream_properties(stream, drm):
+    '''Format parameters to be sent to InputStream list item'''
+    license_types = {
+        'widevine': 'com.widevine.alpha'
+    }
+
+    license_type = license_types.get(drm)
+    path, license_server_url = extract_stream_info(license_type, stream)
+    headers = 'Content-Type=&User-Agent={}&Host={}'.format(USER_AGENT, urlparse(license_server_url).netloc)
+    post_data = 'R{SSM}'
+    response = ''
+    license_key = '{}|{}|{}|{}'.format(license_server_url, headers, post_data, response)
+
+    return path, 'mpd', license_type, license_key
+
+def build_channel_listitem(channel_id):
+    '''Retreive stream information and load InputStream'''
+    stream = get_channel_stream(channel_id)
+    drm = 'widevine'
+
+    if not stream:
+        dialog('This channel is not part of your current registration.')
+        return
+
+    path, manifest_type, license_type, license_key = format_inputstream_properties(stream, drm)
+    log(license_key, xbmc.LOGDEBUG)
+
+    is_helper = inputstreamhelper.Helper(manifest_type, drm=drm)
+    if not is_helper.check_inputstream():
+        return False
+
+    listitem = xbmcgui.ListItem(path=path)
+    listitem.setMimeType('application/xml+dash')
+    listitem.setProperty('inputstream', 'inputstream.adaptive')
+    listitem.setProperty('inputstream.adaptive.manifest_type', manifest_type)
+    listitem.setProperty('inputstream.adaptive.license_type', license_type)
+    listitem.setProperty('inputstream.adaptive.license_key', license_key)
+
+    return listitem
